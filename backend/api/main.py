c3d6962a1a9926701ab2a7233a464982388827d2
joaocruzs <joao.cruz.jc@ufpi.edu.br>
2026-07-09 04:07:22 -0300

    fix: barra

--- backend/api/main.py
+++ backend/api/main.py
@@ -28,7 +28,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:5173",
-        "https://reconhecedor-de-libras.vercel.app/",
+        "https://reconhecedor-de-libras.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
